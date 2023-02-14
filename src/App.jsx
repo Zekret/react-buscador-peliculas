0a790d804c0d45e3f66884c5eef82a5259769ab8
Zekret <benjaminconcha5@gmail.com>
2023-02-14 17:00:50 -0300

    update: list movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import withResults from './mocks/with-results.json'
+import withoutResults from './mocks/not-results.json'
 
 function App() {
-
+  const movies = withResults.Search
+  const hasMovies = movies?.length > 0
   return (
     <div className='page'>
       <h1>Buscador de peliculas</h1>
@@ -12,7 +15,26 @@ function App() {
         </form>
       </header>
       <main>
-        {/* Peliculas */}
+        {
+          hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          :
+          (
+            <p>No se encontraron peliculas para esta busqueda</p>
+          )
+        }
       </main>
     </div>
   )
