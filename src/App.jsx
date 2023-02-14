606f5e0de25a251dffa128179988e6b234d8dd26
Zekret <benjaminconcha5@gmail.com>
2023-02-14 19:04:20 -0300

    update: hook useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useSearch } from './hooks/useSearch'
+
+
 
 function App() {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
   
+  const handleSubmit = (event) => {
+    event.preventDefault()
+   console.log({ search })
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
+
+ 
 
   return (
     <div className='page'>
       <h1>Buscador de peliculas</h1>
       <header>
-        <form className='form'>
-          <input placeholder='Los Vengadores, Harry Potter, Matrix...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={search} name='query' placeholder='Los Vengadores, Harry Potter, Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div >
   )
