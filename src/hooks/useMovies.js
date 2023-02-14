722dd1349701bd5f764f23b420b884f0d221784e
Zekret <benjaminconcha5@gmail.com>
2023-02-14 19:21:23 -0300

    update: search movies function

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,8 +1,11 @@
 import withResults from '../mocks/with-results.json'
 import withoutResults from '../mocks/not-results.json'
+import { useState } from 'react'
 
-export function useMovies () {
-    const movies = withResults.Search
+export function useMovies ({ search }) {
+    const [responseMovies, setResponseMovies] = useState([])
+
+    const movies = responseMovies.Search
   
     const mappedMovies = movies?.map(movie => ({
       id: movie.imdbID,
@@ -10,6 +13,14 @@ export function useMovies () {
       year: movie.Year,
       poster: movie.Poster
     }))
+
+    const getMovies = () => {
+        if (search) {
+            setResponseMovies(withResults)
+        } else {
+            setResponseMovies(withoutResults)
+        }
+    }
   
-    return { movies: mappedMovies }
+    return { movies: mappedMovies, getMovies }
   }
