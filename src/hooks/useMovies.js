091ed93f7e249b1ce555feece8cacb14d4778b58
Zekret <benjaminconcha5@gmail.com>
2023-02-14 19:45:27 -0300

    update: movies services

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,30 +1,23 @@
-import withResults from '../mocks/with-results.json'
-import withoutResults from '../mocks/not-results.json'
 import { useState } from 'react'
+import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
-    const [responseMovies, setResponseMovies] = useState([])
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-    const movies = responseMovies.Search
-  
-    const mappedMovies = movies?.map(movie => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster
-    }))
-
-    const getMovies = () => {
-        if (search) {
-            fetch(`https://www.omdbapi.com/?apikey=676004de&s=${search}`)
-                .then(res => res.json())
-                .then(json => {
-                    setResponseMovies(json)
-                })
-        } else {
-            setResponseMovies(withoutResults)
-        }
+    const getMovies = async () => {
+       try {
+        setLoading(true)
+        setError(null)
+        const newMovies = await searchMovies({ search })
+        setMovies(newMovies)
+       } catch (e) {
+        setError(e.message)
+       } finally {
+        setLoading(false)
+       }
     }
   
-    return { movies: mappedMovies, getMovies }
+    return { movies, getMovies }
   }
